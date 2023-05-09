75032d4eae88574d73bcce3e3f483bd19f44dc50
OleksandrKupchuk <55634677+OleksandrKupchuk@users.noreply.github.com>
2023-05-09 21:15:26 +0300

    The task was implemented in accordance with the requirements: 1. Створити новий IntelliJ-проект 2. Скопіювати у папку src проекту з пункту 1 все, що лежить у папці lection 16 exceptions/src (link). У проекті має лежати 6 класів: WrongAccountException, WrongCurrencyException, WrongOperationException,Account, BankApplication, Main 3. У Main методі main класу створити обєкт типу BankApplication 4. Викликати метод process з наступними параметрами:   4.1 id = accountId000, amount 50, currency USD   4.2 id = accountId003, amount 250, currency HRV   4.3 id = accountId001, amount 50, currency EUR   4.4 id = accountId001, amount 50, currency USD   4.4 id = accountId001, amount 50, currency USD 5. Кожен метод огорнути в try catch finally структуру і для кожного з WrongAccountException, WrongCurrencyException, WrongOperationException, Exception виводити в консоль змістовне повідомлення:   Такого акаунту не існує   Акаунт має рахунок в іншій валюті   Акаунт не має достатньо коштів   Сталася помилка при процесінгу, спробуйте ще раз У finally потрібно виводити повідомлення:   Дякуємо, що скористалися нашим сервісом

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
+import exceprions.WrongAccountException;
+import exceprions.WrongCurrencyException;
+import exceprions.WrongOperationException;
+
 public class Main {
 
     public static void main(String[] args) {
         BankApplication bankApplication = new BankApplication();
-
+        try {
+            bankApplication.process("accountId000", 50, "USD");
+            bankApplication.process("accountId003", 250, "HRV");
+            bankApplication.process("accountId001", 50, "EUR");
+            bankApplication.process("accountId001", 50, "USD");
+            bankApplication.process("accountId001", 50, "USD");
+        } catch (WrongAccountException wrongAccountException){
+            System.out.println("No such account exists");
+        } catch (WrongCurrencyException wrongCurrencyException){
+            System.out.println("The account has a account in another currency");
+        } catch (WrongOperationException wrongOperationException){
+            System.out.println("The account does not have enough funds");
+        } catch (Exception exception) {
+            System.out.println("A processing error occurred, please try again");
+        } finally {
+            System.out.println("Thank you for using our service");
+        }
     }
 }
