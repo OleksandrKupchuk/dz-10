3829afcf40174d9d703013899a2b4cbd38b471a5
OleksandrKupchuk <55634677+OleksandrKupchuk@users.noreply.github.com>
2023-05-09 21:40:48 +0300

    reduced code writing

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,37 @@ import exceprions.WrongAccountException;
 import exceprions.WrongCurrencyException;
 import exceprions.WrongOperationException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         BankApplication bankApplication = new BankApplication();
-        try {
-            bankApplication.process("accountId000", 50, "USD");
-            bankApplication.process("accountId003", 250, "HRV");
-            bankApplication.process("accountId001", 50, "EUR");
-            bankApplication.process("accountId001", 50, "USD");
-            bankApplication.process("accountId001", 50, "USD");
-        } catch (WrongAccountException wrongAccountException){
-            System.out.println("No such account exists");
-        } catch (WrongCurrencyException wrongCurrencyException){
-            System.out.println("The account has a account in another currency");
-        } catch (WrongOperationException wrongOperationException){
-            System.out.println("The account does not have enough funds");
-        } catch (Exception exception) {
-            System.out.println("A processing error occurred, please try again");
-        } finally {
-            System.out.println("Thank you for using our service");
+        List<Account> accounts = new ArrayList<>(){
+            {
+                add(new Account("accountId000", 50, "USD"));
+                add(new Account("accountId003", 250, "HRV"));
+                add(new Account("accountId001", 50, "EUR"));
+                add(new Account("accountId001", 50, "USD"));
+                add(new Account("accountId001", 50, "USD"));
+            }
+        };
+
+        for (Account account : accounts) {
+            try {
+                bankApplication.process(account.getId(), account.getBalance(), account.getCurrency());
+            } catch (WrongAccountException wrongAccountException) {
+                System.out.println("No such account exists");
+            } catch (WrongCurrencyException wrongCurrencyException) {
+                System.out.println("The account has a account in another currency");
+            } catch (WrongOperationException wrongOperationException) {
+                System.out.println("The account does not have enough funds");
+            } catch (Exception exception) {
+                System.out.println("A processing error occurred, please try again");
+            } finally {
+                System.out.println("Thank you for using our service");
+            }
         }
     }
 }
